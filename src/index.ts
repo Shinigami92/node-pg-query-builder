a4c5807dfa93eee390f8a9c02a1a0aa10f14c99b
Christopher Quadflieg <chrissi92@hotmail.de>
2018-07-10 19:35:54 +0200

    export everything from one file

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// DataTypes
+export { DataType, cast } from './data-types';
+
+// Functions
+export { count } from './functions';
+export { to_tsquery, to_tsvector, ts_rank_cd, tsvector_matches_tsquery } from './functions/text-search';
+
+// Logical Operators
+export { and } from './operators/logical';
+
+// Comparison Operators
+export { eq, ge } from './operators/comparison';
+
+// Queries
+export { select } from './query';
+export { QueryBuilder, Aliasable, ToSQLConfig } from './query/query';
+export { SelectQueryBuilder } from './query/select';
+export { FromQueryBuilder, Join, JoinType } from './query/from';
+export { WhereQueryBuilder } from './query/where';
+export { OrderByQueryBuilder, Order } from './query/order';
