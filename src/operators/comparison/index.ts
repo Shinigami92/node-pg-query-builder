ad7ea0e211c4e93faa9bf3040adb935cf9bc0007
Christopher Quadflieg <chrissi92@hotmail.de>
2018-07-10 19:35:21 +0200

    added ge comparison operator
    values can also be number

--- src/operators/comparison/index.ts
+++ src/operators/comparison/index.ts
@@ -1,3 +1,7 @@
-export function eq(column: string, value: string): string {
+export function eq(column: string, value: string | number): string {
 	return `${column} = ${value}`;
+}
+
+export function ge(column: string, value: string | number): string {
+	return `${column} >= ${value}`;
 }
